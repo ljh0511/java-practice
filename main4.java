e2208a015e665a0fd2c291ea0ec33c24f1f091f9
ljh0511 <102738407+ljh0511@users.noreply.github.com>
2022-06-10 12:55:56 +0900

    A+B

---
+++ main4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int c = sc.nextInt();
+		int num[] = new int[c];
+		
+		for(int i = 0; i<c; i++){
+			
+		}
+
+	}
+
+}
