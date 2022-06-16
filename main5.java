d92cad35adcd8e9fd8328cd636cd7c1da861cf5f
ljh0511 <102738407+ljh0511@users.noreply.github.com>
2022-06-16 12:57:31 +0900

    A+B 2

---
+++ main5.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class main3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int c = sc.nextInt();
+		
+		for(int i = 0; i<c; i++){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+		
+	}
+
+}
