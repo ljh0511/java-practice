8ad14ee4c1f549c781ac40d3e685faf657d6b225
ljh0511 <102738407+ljh0511@users.noreply.github.com>
2022-06-23 12:37:52 +0900

    역 N찍기

---
+++ n2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class n2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num =sc.nextInt();
+		
+		for(int i=num; i>=1; i--){
+			System.out.println(i);
+		}
+
+	}
+
+}
