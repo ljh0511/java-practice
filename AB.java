936bf38ba1093d04012ffef855ebc9abe0d964fb
ljh0511 <102738407+ljh0511@users.noreply.github.com>
2022-06-24 12:57:55 +0900

    A+B 3

---
+++ AB.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AB {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int c = sc.nextInt();
+		for(int i=1; i<=c; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println("Case #"+ i +":"+ (a+b));
+		}
+
+	}
+
+}
